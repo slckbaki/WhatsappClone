e7e9069942a6771ba2125c081960627d056ca72e
selcuk baki <selcukbaki@windowslive.com>
2021-10-16 03:19:37 +0300

    Initial Commit

---
+++ WhatsappClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsappClone
+//
+//  Created by Selcuk Baki on 16/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsappClone/WhatsappCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCloneApp.swift
+//  WhatsappClone
+//
+//  Created by Selcuk Baki on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
